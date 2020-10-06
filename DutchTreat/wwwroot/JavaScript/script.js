98c9c3c149439a514de7378868de31ab3df57804
DesMabusela <desree@tshimologong.joburg>
2020-10-06 21:37:18 +0200

    commit all

---
+++ DutchTreat/wwwroot/JavaScript/script.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    var TheForm = $("#Form");
+    TheForm.hide();
+
+    var button = $("buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var ProductInfo = $(".ProductProps li");
+    ProductInfo.on("click", function () {
+        console.log("you clicked on" + $(this).text());
+    })
+});
